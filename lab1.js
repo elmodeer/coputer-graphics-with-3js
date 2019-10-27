bb9aff5a170b03e4989c28bb375dea2e32995835
Hesham Hussen <hesham.200053@gmail.com>
2019-10-27 07:03:00 +0100

    refactored variable names

--- lab1.js
+++ lab1.js
@@ -1,6 +1,6 @@
 let singleMode = false;
 let doubleMode = false;
-
+let gameOver = false;
 (confirm('Welcome to silly game .. Do you want to play in DoubleMode?')) ?
   doubleMode = true : singleMode = true;
 
@@ -19,56 +19,59 @@ camera.lookAt(scene.position);   // camera looks at origin
 const ambientLight = new THREE.AmbientLight("white");
 scene.add(ambientLight);
 
+// useful variables and constants
+var collidableMeshList = [];
+const playGroundWidth = 8;
+const playGroundHeight = 16;
+// the distance between the ball radious and the cushion, which is basically the ball radious
+const ballRadious = 0.3;
+const cushionWidth = 0.2;
+
+
 // 1- playgorund
 const playgorund = new THREE.Object3D();
 const planeMat = new THREE.MeshBasicMaterial( {color: "green", side: THREE.DoubleSide} );
-const planeGeo = new THREE.PlaneGeometry( 8, 16 );
+const planeGeo = new THREE.PlaneGeometry( playGroundWidth, playGroundHeight);
 const plane = new THREE.Mesh( planeGeo, planeMat );
 // plane.position.set(0,8,0);
 
 // 2- line in the middle
 const lineMat = new THREE.LineBasicMaterial( { color: "white" } );
 const lineGeo = new THREE.Geometry();
-lineGeo.vertices.push(new THREE.Vector3(-4,0,0));
+lineGeo.vertices.push(new THREE.Vector3(-playGroundWidth/2,0,0));
 lineGeo.vertices.push(new THREE.Vector3(0,0,0));
-lineGeo.vertices.push(new THREE.Vector3(4,0,0));
+lineGeo.vertices.push(new THREE.Vector3(playGroundWidth/2,0,0));
 const line = new THREE.Line(lineGeo, lineMat);
 playgorund.add(line);
 
 // 3- add the cushions
 // first cushion
-var collidableMeshList = [];
-const playGroundWidth = 3.8;
-const playGroundHeight = 7.8;
-// the distance between the ball radious and the cushion, which is basically the ball radious
-const ballRadious = 0.3;
-const cushionWidth = 0.2;
-const cushion1Geo = new THREE.BoxGeometry( cushionWidth, 16, 1 );
+const cushion1Geo = new THREE.BoxGeometry( cushionWidth , playGroundHeight, 1 );
 const cushion1Mat = new THREE.MeshBasicMaterial( {color: "#006400"} );
 const cushion1 = new THREE.Mesh( cushion1Geo, cushion1Mat );
-cushion1.position.set(-3.9,0,0.5);
+cushion1.position.set(-(playGroundWidth/2 - cushionWidth/2), 0, 0.5);
 playgorund.add(cushion1);
 
 // second cushion
-const cushion2Geo = new THREE.BoxGeometry( cushionWidth, 16, 1 );
+const cushion2Geo = new THREE.BoxGeometry( cushionWidth, playGroundHeight, 1 );
 const cushion2Mat = new THREE.MeshBasicMaterial( {color: "#006400"} );
 const cushion2 = new THREE.Mesh( cushion2Geo, cushion2Mat );
-cushion2.position.set(3.9,0,0.5);
+cushion2.position.set(playGroundWidth/2 - cushionWidth/2, 0, 0.5);
 playgorund.add(cushion2);
 
 // 4- cushion for singleMode
-const cushionSMGeo = new THREE.BoxGeometry( cushionWidth, 7.8, 1 );
+const cushionSMGeo = new THREE.BoxGeometry( cushionWidth, playGroundWidth - cushionWidth, 1 );
 const cushionSMMat = new THREE.MeshBasicMaterial( {color: "#006400"} );
 const cushionSM = new THREE.Mesh( cushionSMGeo, cushionSMMat );
 cushionSM.rotation.z = -Math.PI/2;
-cushionSM.position.set(0,7.9,0.5);
+cushionSM.position.set(0, playGroundHeight/2 - cushionWidth/2, 0.5);
 
 
-const cushionDMGeo = new THREE.BoxGeometry( cushionWidth, 7.8, 1 );
+const cushionDMGeo = new THREE.BoxGeometry( cushionWidth, playGroundWidth - cushionWidth, 1 );
 const cushionDMMat = new THREE.MeshBasicMaterial( {color: "#006400"} );
 const cushionDM = new THREE.Mesh( cushionDMGeo, cushionDMMat );
 cushionDM.rotation.z = -Math.PI/2;
-cushionDM.position.set(0,-7.9,0.5);
+cushionDM.position.set(0,-(playGroundHeight/2 - cushionWidth/2),0.5);
 // playgorund.add(cushionDM);
 
 // 5- box for player 1
@@ -77,7 +80,7 @@ const player1Geo = new THREE.BoxGeometry( cushionWidth, playerHeight, 1 );
 const player1Mat = new THREE.MeshBasicMaterial( {color: "red"} );
 const player1 = new THREE.Mesh( player1Geo, player1Mat );
 player1.rotation.z = -Math.PI/2;
-player1.position.set(0,-7.9,0.5);
+player1.position.set(0,-(playGroundHeight/2 - cushionWidth/2),0.5);
 playgorund.add(player1);
 
 // 6- add player 2
@@ -85,7 +88,7 @@ const player2Geo = new THREE.BoxGeometry( cushionWidth, playerHeight, 1 );
 const player2Mat = new THREE.MeshBasicMaterial( {color: "blue"} );
 const player2 = new THREE.Mesh( player2Geo, player2Mat );
 player2.rotation.z = -Math.PI/2;
-player2.position.set(0,7.9,0.5);
+player2.position.set(0,(playGroundHeight/2 - cushionWidth/2),0.5);
 
 // if double mode activate
 (doubleMode) ? playgorund.add(player2): playgorund.add(cushionSM);
@@ -98,7 +101,6 @@ const ball = new THREE.Mesh(ballGeo, ballMat);
 ball.position.z = ballRadious;
 scene.add(ball);
 let speed = 0.06;
-// let speed = 1;
 
 
 
@@ -110,7 +112,7 @@ const movePlayerOne = function(event) {
   const rightKeystroke = 39;
   const aKeyStroke = 65;
   const dKeyStroke = 68;
-  const editedPlayGroundWitdth = playGroundWidth -1;
+  const editedPlayGroundWitdth = playGroundWidth/2 -1;
 
   (event.keyCode === rightKeystroke) ?
     (player1.position.x < editedPlayGroundWitdth)  ? player1.position.x += 0.5 : null : null;
@@ -131,13 +133,11 @@ const detectCollision  = function() {
   // console.log("player: " + player1.position);
   // console.log("ball: " + ball.position);
   let d = player1.position.distanceToSquared(ball.position);
-  if((playGroundHeight - ball.position.y ) < ballRadious  ) {
+  if((playGroundHeight/2 - ball.position.y ) < ballRadious  ) {
       direction = reflect('y');
-  } else if((playGroundHeight + ball.position.y ) < ballRadious  ) {
+  } else if((playGroundHeight/2 + ball.position.y ) < ballRadious  ) {
       reflectCorrespondingly();
-        // if ((Math.abs(ball.position.x) - (Math.abs(player1.position.x)+ 0.3) ) < collisonThreshold )
-        //     direction = reflect('y');
-  } else if((playGroundWidth - Math.abs(ball.position.x)) < ballRadious) {
+  } else if((playGroundWidth/2 - Math.abs(ball.position.x)) < ballRadious) {
       direction = reflect('x');
   }
 }
