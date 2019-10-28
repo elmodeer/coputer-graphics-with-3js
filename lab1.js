c184cfca19621a5306b6dfff1bd07a9139012b20
Hesham Hussen <hesham.200053@gmail.com>
2019-10-28 18:00:40 +0100

    full solution

--- lab1.js
+++ lab1.js
@@ -51,7 +51,6 @@ lineGeo.vertices.push(new THREE.Vector3(-playGroundWidth/2,0,0));
 lineGeo.vertices.push(new THREE.Vector3(0,0,0));
 lineGeo.vertices.push(new THREE.Vector3(playGroundWidth/2,0,0));
 const line = new THREE.Line(lineGeo, lineMat);
-line.scale.set( 1, 1, 1 );
 playgorund.add(line);
 
 let Side = {
@@ -60,6 +59,7 @@ let Side = {
     LEFT: -1,
     RIGHT: 1
 };
+
 const Cushion = {
     construct: function(width, height, depth) {
       const cushionGeo = new THREE.BoxGeometry( width , height, depth);
@@ -69,6 +69,7 @@ const Cushion = {
     }
 }
 
+const editedPlayGroundWitdth = playGroundWidth/2 -1;
 const Player = {
     construct: function(color, side) {
       const player1Geo = new THREE.BoxGeometry( cushionWidth, playerHeight, 1 );
@@ -77,11 +78,38 @@ const Player = {
       player.rotation.z = -Math.PI/2;
       player.position.set(0, side * (playGroundHeight/2 - cushionWidth/2), cushionDepth);
       return player;
+    },
+    speed:0.5,
+    movementStep: 0.5,
+    move: function() {
+      const leftKeystroke = 37;
+      const rightKeystroke = 39;
+      const aKeyStroke = 65;
+      const dKeyStroke = 68;
+      if (!gameOver) {
+        if (keyPressed=== rightKeystroke)
+          if (player1.position.x < editedPlayGroundWitdth)
+            player1.position.x += Player.speed * Player.movementStep;
+
+        if (keyPressed=== leftKeystroke)
+          if (player1.position.x > -editedPlayGroundWitdth)
+            player1.position.x -= Player.speed * Player.movementStep;
+
+        if (keyPressed=== aKeyStroke)
+          if (player2.position.x < editedPlayGroundWitdth)
+            player2.position.x += Player.speed * Player.movementStep;
+
+        if (keyPressed === dKeyStroke)
+          if (player2.position.x > -editedPlayGroundWitdth)
+            player2.position.x -= Player.speed * Player.movementStep;
+      }
     }
 }
 
 // add the ball
+
 const Ball = {
+    speed: 0.06,
     construct : function(r) {
       const ballGeo = new THREE.SphereGeometry(r, 16,16);
       const ballMat = new THREE.MeshBasicMaterial({color: "yellow",
@@ -91,8 +119,8 @@ const Ball = {
       return ballObj;
     },
     move: function() {
-      ball.position.x += speed * direction.x;
-      ball.position.y += speed * direction.y;
+      ball.position.x += Ball.speed * direction.x;
+      ball.position.y += Ball.speed * direction.y;
     }
 }
 const ball = Ball.construct(ballRadious);
@@ -101,14 +129,12 @@ const player2 = Player.construct("blue",Side.UP);
 const cushion1 = Cushion.construct(cushionWidth, playGroundHeight, playGroundDepth);
 const cushion2 = Cushion.construct(cushionWidth, playGroundHeight, playGroundDepth);
 const cushionSM = Cushion.construct( cushionWidth, playGroundWidth - cushionWidth, playGroundDepth);
-const cushionDM = Cushion.construct( cushionWidth, playGroundWidth - cushionWidth, playGroundDepth);
 
 cushion1.position.set(Side.LEFT * widthAlignment, 0, cushionDepth);
 cushion2.position.set(Side.RIGHT * widthAlignment, 0, cushionDepth);
 cushionSM.position.set(0, Side.UP * heightAlignment, cushionDepth);
 cushionSM.rotation.z = -Math.PI/2;
-// cushionDM.position.set(0,Side.LEFT * heightAlignment, cushionDepth);
-// cushionDM.rotation.z = -Math.PI/2;
+
 
 // playgorund components
 playgorund.add(cushion1);
@@ -118,40 +144,15 @@ playgorund.add(player1)
 // scene components
 scene.add(playgorund);
 scene.add(ball);
-let speed = 0.06;
 
 
 
 // functions
 // movement part
-const movePlayerOne = function(event) {
-  console.log(secondCamera.position);
-  console.log(secondCamera.rotation);
-
-  console.log(firstCamera.position);
-  event.preventDefault();
-  const leftKeystroke = 37;
-  const rightKeystroke = 39;
-  const aKeyStroke = 65;
-  const dKeyStroke = 68;
-  const editedPlayGroundWitdth = playGroundWidth/2 -1;
-  const movementStep = 0.5;
-  (event.keyCode === rightKeystroke) ?
-    (player1.position.x < editedPlayGroundWitdth)  ?
-     player1.position.x += movementStep : null : null;
-
-  (event.keyCode === leftKeystroke) ?
-    (player1.position.x > -editedPlayGroundWitdth) ?
-     player1.position.x -= movementStep : null : null;
-
-  (event.keyCode === aKeyStroke) ?
-    (player2.position.x < editedPlayGroundWitdth)  ?
-     player2.position.x += movementStep : null : null;
-
-  (event.keyCode === dKeyStroke) ?
-    (player2.position.x > -editedPlayGroundWitdth) ?
-     player2.position.x -= movementStep : null : null;
-
+let keyPressed = 0;
+const movePlayer = function(event) {
+  Player.speed = 0.5;
+  keyPressed = event.keyCode;
 };
 
 
@@ -200,31 +201,36 @@ const reload = function(){
   location.reload();
 }
 const singleModeActiviate = function() {
+  if (!gameOver) {
     singleMode = true;
     doubleMode = false;
     secondCanvas.style.display = "none";
     playgorund.add(cushionSM);
     playgorund.remove(player2);
+  }
 }
 
 const doubleModeActiviate = function() {
+  if (!gameOver) {
     doubleMode = true;
     singleMode = false;
     secondCanvas.style.display = "block";
     playgorund.add(player2);
     playgorund.remove(cushionSM);
+  }
 }
-function degInRad(deg) {
-    return deg * Math.PI / 180;
-}
-document.addEventListener('keydown', movePlayerOne);
+
+document.addEventListener('keydown', movePlayer);
+document.addEventListener("keyup", (event) => {
+    Player.speed = 0;
+});
 const fcontrols = new THREE.TrackballControls( firstCamera, firstcanvas );
 const scontrols = new THREE.TrackballControls( secondCamera, secondCanvas );
 
-const clock = new THREE.Clock();
 function render() {
   requestAnimationFrame(render);
   detectCollision();
+  Player.move(keyPressed);
   if (!gameOver)
      Ball.move();
   fcontrols.update();
