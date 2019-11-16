cc53d2751108700383beb2e5750fb8b007208119
Hesham Hussen <hesham.200053@gmail.com>
2019-11-16 10:05:53 +0100

    very good state

--- lab2.js
+++ lab2.js
@@ -16,10 +16,13 @@ const clockRadius = 5;
 const clockDepth = 1;
 const elementPlacement = 0.51;
 
+const clock = new THREE.Object3D();
 const clockBodyGeometry = new THREE.CylinderGeometry(clockRadius, clockRadius, clockDepth, 64);
 const clockBodyMaterial = new THREE.MeshBasicMaterial({color: 'white'});
 const clockBody = new THREE.Mesh(clockBodyGeometry, clockBodyMaterial);
-scene.add(clockBody);
+clock.rotation.y = Math.PI;
+clock.add(clockBody);
+// scene.add(clockBody);
 
 var points = [];
 for ( let i = 0; i < 20; i ++ ) {
@@ -28,7 +31,29 @@ for ( let i = 0; i < 20; i ++ ) {
 const geometry = new THREE.LatheGeometry(points);
 const material = new THREE.MeshBasicMaterial({color: 'lightblue'});
 const lathe = new THREE.Mesh(geometry, material);
-scene.add(lathe);
+clock.add(lathe);
+
+// scene.add(lathe);
+
+// seconds
+const secondsHandGeo = new THREE.PlaneGeometry(0.03, 6, 10);
+const secondsHandMat = new THREE.MeshBasicMaterial({color: 0x000fff,  side: THREE.DoubleSide});
+const secondsHand = new THREE.Mesh(secondsHandGeo, secondsHandMat);
+
+secondsHand.position.y = elementPlacement;
+secondsHand.position.x = 0;
+secondsHand.position.z = 0;
+
+secondsHand.rotation.x = Math.PI / 2;
+
+const secondsHand2 = secondsHand.clone();
+secondsHand2.position.y = -1 * elementPlacement;
+
+clock.add(secondsHand);
+clock.add(secondsHand2);
+
+// scene.add(secondsHand);
+// scene.add(secondsHand2);
 
 const Hand = {
   construct: function(width, height, depth) {
@@ -40,7 +65,7 @@ const Hand = {
     hand.rotation.x = Math.PI / 2;
     return hand;
   }
-}
+};
 
 const Blob = {
   construct: function(width, height, depth) {
@@ -49,9 +74,9 @@ const Blob = {
     const blob = new THREE.Mesh(blobGeo, blobMat);
     return blob;
   }
-}
+};
 
-//second
+//hour
 const hoursHand = Hand.construct(1.5, 32, 32)
 hoursHand.position.y = elementPlacement;
 hoursHand.position.z =  -1;
@@ -61,6 +86,7 @@ hoursHand.position.z =  -1;
 const minutesHand = Hand.construct(2, 32, 32)
 minutesHand.position.y = elementPlacement;
 minutesHand.position.z = 2;
+
 // minutesHand.rotation.x = Math.PI / 2;
 
 const hoursHand2 = hoursHand.clone();
@@ -75,30 +101,60 @@ const backBlob = frontBlob.clone();
 backBlob.position.y = - 1 * elementPlacement;
 
 
+clock.add(hoursHand);
+clock.add(hoursHand2);
+clock.add(minutesHand);
+clock.add(minutesHand2);
 
-scene.add(hoursHand);
-scene.add(hoursHand2);
-scene.add(minutesHand);
-scene.add(minutesHand2);
+clock.add(frontBlob);
+clock.add(backBlob);
 
-scene.add(frontBlob);
-scene.add(backBlob);
+// scene.add(hoursHand);
+// scene.add(hoursHand2);
+// scene.add(minutesHand);
+// scene.add(minutesHand2);
+//
+// scene.add(frontBlob);
+// scene.add(backBlob);
 
 drawTicks();
+// setInitialPosition();
 
+// function setInitialPosition() {
+//   const now = new Date();
+//   const hrs = now.getHours();
+//   const min = now.getMinutes();
+//   const sec = now.getSeconds();
+//   // console.log(sec);
+//
+//   const handHourR = (30 * (hrs > 12 ? hrs - 12 : hrs) * Math.PI) / 180;
+//   const relativeHourR = ((30 * min / 60) * Math.PI) / 180;
+//   const handMinuteR = (6 * min * Math.PI) / 180;
+//   const handSecondR = (6 * sec * Math.PI) / 180;
+//   moveHand(secondsHand, handSecondR);
+//   moveHand(minutesHand, handMinuteR);
+//   moveHand(hoursHand, (handHourR + relativeHourR));
+//
+// };
+function moveHand(secondsHand, handSecondR) {
+  secondsHand.rotation.z = handSecondR;
+  secondsHand.position.z = Math.cos(-handSecondR);
+  secondsHand.position.x = Math.sin(-handSecondR);
+};
 function drawTicks() {
-  const width = 0.1;
+  const width = 0.05;
+  const widthTens = 0.11;
   const heightTens = 0.7;
   let height = 0.5;
   for (let i=0; i<60; i++) {
     let rotationAngle =  2 * i * Math.PI / 60;
     if(i == 30 || i == 0 || i == 15 || i == 45 || (i%5 == 0)){
-      drawTick(rotationAngle, 'black', heightTens, width);
+      drawTick(rotationAngle, 'black', heightTens, widthTens);
     } else {
       drawTick(rotationAngle, 'red', height, width);
     }
   }
-}
+};
 
 function drawTick(rotationAngle, color, height, width) {
   const tickGeometry = new THREE.PlaneGeometry( width , height, 32 );
@@ -115,14 +171,39 @@ function drawTick(rotationAngle, color, height, width) {
 
   const tickPlaneBack = tickPlaneFront.clone();
   tickPlaneBack.position.y = - 1 * elementPlacement;
-  scene.add(tickPlaneFront);
-  scene.add(tickPlaneBack);
-}
+  clock.add(tickPlaneFront);
+  clock.add(tickPlaneBack);
+};
 
 const controls = new THREE.TrackballControls( camera, canvas );
-
+scene.add(clock);
 function render() {
   requestAnimationFrame(render);
+  var date = new Date();
+  var hrs = date.getHours();
+	var min = date.getMinutes();
+	var sec = date.getSeconds();
+  // console.log(sec);
+	var handHourR = (30 * (hrs > 12 ? hrs - 12 : hrs) * Math.PI) / 180;
+  const relativeHourR = ((30 * min / 60) * Math.PI) / 180;
+
+	var handMinuteR = (6 * min * Math.PI) / 180;
+	var handSecondR = (6 * sec * Math.PI) / 180;
+
+	// hoursHand.rotation.z = -handHourR;
+	// minutesHand.rotation.z = -handMinuteR;
+  // why
+	moveHand(secondsHand, handSecondR);
+  // moveHand(secondsHand2, - handSecondR);
+  moveHand(minutesHand, handMinuteR);
+  moveHand(hoursHand, handHourR + relativeHourR);
+
+  // secondsHand.position.z = -handSecondR;
+
+  // secondsHand.rotation.z = (Date.now()/60000)%60 * 6;
+  // secondsHand.position.z = 2 * Math.cos(-(Date.now()/60000)%60 * 6);
+  // secondsHand.position.x = 2 * Math.sin(-(Date.now()/60000)%60 * 6);
+
   controls.update();
   renderer.render(scene, camera);
 }
